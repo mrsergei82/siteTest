f3758307019fcede7a721b6654710d0084729dba
Sergei <mr.sergei82@mail.ru>
2021-11-19 20:51:06 +0300

    1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -26,7 +26,18 @@ public class App
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://diary.ru");
+        driver.get("http://bikeland.ru");
+        driver.findElement(By.xpath(
+                "//div[@class='order-1 order-md-2 col col-md-auto col-lg-3 " +
+                        "text-right']/div[@class='header-controls']/a")).click();
+        driver.findElement(By.id("user_login")).sendKeys("SuperSeryi");
+        driver.findElement(By.id("user_password")).sendKeys("s2233s");
+        driver.findElement(By.name("Login")).click();
+
+
+
+
+
 
     }
 
