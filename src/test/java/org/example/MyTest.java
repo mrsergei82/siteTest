ac7892f38735e13adc524d9a36dfda85ccfe1101
Sergei <mr.sergei82@mail.ru>
2021-11-26 22:56:43 +0300

    добавил тесты и асерты

--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -24,6 +24,8 @@ public class MyTest {
         driver.findElement(By.xpath("//div[@class=\"b-loginform-field__wrap\"]/input")).sendKeys("Sergei777777");
         driver.findElement(By.id("lj_loginwidget_password")).sendKeys("SuperGg1");
         driver.findElement(By.name("action:login")).click();
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]/a/span")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://sergei777777.livejournal.com/"));
         driver.close();
     }
     @Test
@@ -38,5 +40,43 @@ public class MyTest {
         driver.findElement(By.xpath("//div[@class=\"b-loginform-field__wrap\"]/input")).sendKeys("Sergei777777");
         driver.findElement(By.id("lj_loginwidget_password")).sendKeys("SuperGg1");
         driver.findElement(By.name("action:login")).click();
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]/a/span")).click();
+        driver.findElement(By.xpath("//ul[@class='j-nav j-header-nav j-journal-nav']/li[2]/a")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://sergei777777.livejournal.com/feed/"));
+        driver.close();
+    }
+    @Test
+    public void test3(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("incognito");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.get("https://www.livejournal.com");
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]")).click();
+        driver.findElement(By.xpath("//div[@class=\"b-loginform-field__wrap\"]/input")).sendKeys("Sergei777777");
+        driver.findElement(By.id("lj_loginwidget_password")).sendKeys("SuperGg1");
+        driver.findElement(By.name("action:login")).click();
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]/a/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"home\"]/div[4]/div[3]/div/div[1]/div/div/header/div[2]/div/ul/li[3]/a")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://sergei777777.livejournal.com/profile"));
+        driver.close();
+    }
+    @Test
+    public void test4(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("incognito");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.get("https://www.livejournal.com");
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]")).click();
+        driver.findElement(By.xpath("//div[@class=\"b-loginform-field__wrap\"]/input")).sendKeys("Sergei777777");
+        driver.findElement(By.id("lj_loginwidget_password")).sendKeys("SuperGg1");
+        driver.findElement(By.name("action:login")).click();
+        driver.findElement(By.xpath("//body/div[2]/header/div[1]/nav[2]/ul/li[2]/a/span")).click();
+        driver.findElement(By.xpath("//a[@title='Избранное']")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://sergei777777.livejournal.com/memories"));
+        driver.close();
     }
 }
